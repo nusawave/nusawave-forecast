a9aa53fa2b845fc5d1c3e7faa911c9d76b7f80a5
yothunder <tyo.suwignyo@gmail.com>
2025-12-03 22:02:55 +0700

    add static params

--- plotter/modelparams/gfswave.py
+++ plotter/modelparams/gfswave.py
@@ -1,5 +1,9 @@
 VARIABLE_MAP = {
+    "time": "time",
+    "lon": "lon",
+    "lat": "lat",
     "swell": {"mag": "swell_1", "dir": "swdir_1"},
     "swh": {"mag": "htsgwsfc", "dir": "dirpwsfc"},
     "wind": {"u": "ugrdsfc", "v": "vgrdsfc"},
+    "source": "GFS Wave 0.25deg"
 }
